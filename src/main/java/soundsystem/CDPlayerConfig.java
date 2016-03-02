755846f23e85971c965885f34211c4d5ba48f236
Fola Ayinde <kamfbd@gmail.com>
2016-03-01 21:09:13 -0500

    Removed annotation

--- src/main/java/soundsystem/CDPlayerConfig.java
+++ src/main/java/soundsystem/CDPlayerConfig.java
@@ -1,10 +1,18 @@
 package soundsystem;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 public class CDPlayerConfig {
 
+	@Bean
+	public CompactDisc sgtPeppers() {
+		return new SgtPeppers();
+	}
+
+	@Bean
+	public CDPlayer cdPlayer() {
+		return new CDPlayer(sgtPeppers());
+	}
 }
